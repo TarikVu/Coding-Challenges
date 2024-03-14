c7069352740fb8ae3a739c51d310baff403db6ff
Tarik Vu <Tarikvu2@yahoo.com>
2024-03-14 14:55:34 -0600

    commenting

--- python/LinkedListCycle.py
+++ python/LinkedListCycle.py
@@ -3,6 +3,13 @@ class ListNode(object):
          self.val = x
          self.next = None
 
+""" 
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer.
+
+Answer:  Floyd's Tortoise and Hare Algorithm
+# referenced article: https://www.linkedin.com/pulse/navigating-racetrack-understanding-floyds-slow-fast-pointers-penrose-mo8ic/
+"""
 def hasCycle(head):
     tortoise = head
     hare = head 
@@ -17,6 +24,7 @@ def hasCycle(head):
 
     return False
 
+# Tests ----------------
 
 # Standard cycle 
 n1  = ListNode(1)
@@ -48,8 +56,7 @@ expectedLen2 = False
 passedLen2 = expectedLen2 == resultLen2
 print("Expected: " + str(expectedLen2) +" | Actual: " + str(resultLen2) + " | Passed: " + str(passedLen2))
 
-# Single Cycle test
-
+# Single Cycle test (cycle on self)
 sc = ListNode(0)
 sc.next = sc
 scres = hasCycle(sc)
