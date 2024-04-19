61c69a2d6f5cf77fd8d3c17dad51f5480250245f
Tarik Vu <Tarikvu2@yahoo.com>
2024-04-19 10:04:58 -0600

    init

--- c_sharp/obj/Debug/net8.0/c_sharp.AssemblyInfo.cs
+++ c_sharp/obj/Debug/net8.0/c_sharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c_sharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7069352740fb8ae3a739c51d310baff403db6ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("c_sharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c_sharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
